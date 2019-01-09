8d927d6f94244ba50c9ed84ba0a487b829f65958
brandongrych <brandongrych@gmail.com>
2019-01-09 12:54:52 -0600

    BouncingRainbow implemented

--- src/ball.py
+++ src/ball.py
@@ -66,11 +66,11 @@ def update(self):
 
          super().update()
 
-# class BouncingRainbow(???):
-#     """
-#     Ball that changes color and is affected by gravity
-#     """
-#     # TODO:
+class BouncingRainbow(BouncingBall,RainbowBall):
+    """
+    Ball that changes color and is affected by gravity
+    """
+    pass
 
 # class KineticBall(???):
 #     """
